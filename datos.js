0547d0f6032b6f9ff5131d20496cf7c33dd570e3
margarita <maguiblaanco94@gmail.com>
2023-03-01 15:13:53 -0300

    main

---
+++ datos.js
@@ -0,0 +1,86 @@
+const products = document.querySelector('.products-container')
+const categories = document.querySelector('.categories')
+const categoriesList = document.querySelectorALL('.category')
+
+
+
+
+
+
+
+
+const hamburguesa = document.querySelector('.hamburguesa');
+const navbar = document.querySelector('.navbar');
+const enlaces = document.querySelectorAll('.navbar a')
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarMenu();
+    cerrarMenu();
+});
+
+function mostrarMenu() {
+    hamburguesa.addEventListener('click', () => {
+        navbar.classList.toggle('ocultar');
+
+    });
+}
+
+
+function cerrarMenu(){
+    enlaces.forEach(enlace =>{
+enlace.addEventListener('click',(e)=>{
+0
+    if(e.target.tagName === 'A'){
+        navbar.classList.add('ocultar');
+    }
+});
+    });
+}
+
+
+const renderProduct = ({id, imagen, titulo, descripcion, precio}) => {
+
+    return `
+    
+    <div class="product">
+    <img src=${imagen} alt="combinado2">
+    <div class="product-info">
+        <div class="product-top">
+            <h2>${titulo} </h2>
+            <p>${descripcion}</p>
+        </div>
+        <div class="product-mid">
+            <span>$${precio}</span>
+        </div>
+        <button class="btn-agregar" 
+        data-id='${id}'
+        data-img='${imagen}'
+        data-titulo='${titulo}'
+        data-desc='${descripcion}'
+        data-precio='${precio}' >Agregar</button>
+    </div>
+</div>
+      
+    ` 
+}
+
+
+
+const renderDividedProducts (index = 0) => {
+    const productsToRender = productsController.dividedProducts[index]
+products.innerHTML = productsToRender.map(renderProduct).join('')
+}
+
+
+const renderProducts = (index = 0, category = null) => {
+if (!category) {
+    renderDividedProducts(index)
+}
+else {}
+} 
+
+const init = () => {
+renderProducts()
+
+
+}
+init    ()
---
+++ main.js
@@ -0,0 +1,115 @@
+
+const productsData = [
+    {
+        id: 1,
+        imagen: "./imagenes/combinado2.jpg",
+        titulo: "Tablas x 15u",
+        descripcion: "Tablas combinadas",
+        precio: $4500,
+        categoria: "Tablas",
+    },
+    {
+        id: 2,
+        imagen: "./imagenes/combinado 30.jpg.jpg",
+        titulo: "Tablas x 30u",
+        descripcion: "Tablas full salmón",
+        precio: $6500,
+        categoria: "Tablas",
+    },
+    {
+        id: 3,
+        imagen: "./imagenes/hotroll1.png",
+        titulo: "Hot roll Cuba",
+        descripcion: "Roll rebozado relleno de arroz, queso y salmon cocido.",
+        precio: $950,
+        categoria: "Hot Roll",
+    },
+    {
+        id: 4,
+        imagen: "./imagenes/hotrolls3.jpg",
+        titulo: "Hot roll Mexico",
+        descripcion: "Roll relleno de arroz, salmon grill con salsa de mango.",
+        precio: $900,
+        categoria: "Hot Roll",
+    },
+    {
+        id: 5,
+        imagen: "./imagenes/hotroll4.jpg",
+        titulo: "Hot roll Persia",
+        descripcion: "Roll relleno de arroz, langostino y palta envuelto en salmón.",
+        precio: $880,
+        categoria: "Hot Roll",
+    },
+    {
+        id: 6,
+        imagen: "./imagenes/hotroll2.jpg",
+        titulo: "Hot roll Caribe",
+        descripcion: "Roll relleno de langostino con arroz rebozado con caviar.",
+        precio: $980,
+        categoria: "Hot Roll",
+    },
+    {
+        id: 7,
+        imagen: "./imagenes/11.jpg",
+        titulo: "Roll Bs As",
+        descripcion: "Roll relleno de salmón cocido, verdeo y arroz.",
+        precio: $850,
+        categoria: "Rolls",
+    },
+    {
+        id: 8,
+        imagen: "./imagenes/13.jpg",
+        titulo: "Roll New York",
+        descripcion: "Roll relleno de salmón, palta y queso Philadelphia.",
+        precio: $890,
+        categoria: "Rolls",
+    },
+    {
+        id: 9,
+        imagen: "./imagenes/14.jpg",
+        titulo: "Roll Hawai",
+        descripcion: "Roll relleno de arroz, mango y queso Philadelphia.",
+        precio: $850,
+        categoria: "Rolls",
+    },
+    {
+        id: 10,
+        imagen: "./imagenes/14.jpg",
+        titulo: "Roll Hawai",
+        descripcion: "Roll relleno de arroz, mango y queso Philadelphia.",
+        precio: $850,
+        categoria: "Rolls",
+    },
+    {
+        id: 11,
+        imagen: "./imagenes/4.jpg",
+        titulo: "Nigiri salmón",
+        descripcion: "Bocadito de arroz envuelto en salmón",
+        precio: $850,
+        categoria: "Rolls",
+    },
+    {
+        id: 12,
+        imagen: "./imagenes/15.jpg",
+        titulo: "Roll California",
+        descripcion: "Roll relleno de arroz, palta y atún.",
+        precio: $900,
+        categoria: "Rolls",
+    },
+];
+
+
+
+const splitProducts = size => {
+    let dividedProducts = [];
+    for (let i = 0; i < productsData.lenght; i += size) {
+        dividedProducts.push(productsData.slice(i, i + size))
+    }
+    return dividedProducts;
+}
+
+const productsController = {
+    dividedProducts: splitProducts(6),
+    nextProductsIndex: 1,
+    productsLimit: splitProducts(6).length
+}
